755fd986751d0f3b0d7772d4166e8bb0ec3c73b8
sam22mutabazi <142934954+sam22mutabazi@users.noreply.github.com>
2024-12-11 16:07:42 +0200

    secured codes/djangoproject3
    secured codes after manual code review

--- urls.py
+++ urls.py
@@ -1,9 +1,7 @@
-from django.urls import path
-from . import views
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('register/', views.register, name='register'),
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),  # Logout URL
+    path('admin/', admin.site.urls),
+    path('', include('my_app.urls')),  # Include the URLs from 'my_app'
 ]
