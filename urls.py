bb5aaae2dec1553a5059a8316b02315bacbe8a75
sam22mutabazi <142934954+sam22mutabazi@users.noreply.github.com>
2024-12-11 15:55:03 +0200

    secured codes/myapp
    secured codes after manual code review

--- urls.py
+++ urls.py
@@ -1,7 +1,9 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),  # Admin panel URL
-    path('', include('myapp.urls')),  # Include the app's URLs
+    path('', views.home, name='home'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),  # Logout URL
 ]
