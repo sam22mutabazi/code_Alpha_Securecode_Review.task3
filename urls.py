acf739c479d0f25a09e6b5f6257ec01c960f4104
sam22mutabazi <142934954+sam22mutabazi@users.noreply.github.com>
2024-12-11 15:50:03 +0200

    vulnerable codes/djangoproject4
    vulnerable codes before manual code review

--- urls.py
+++ urls.py
@@ -1,9 +1,7 @@
-from django.urls import path
-from . import views
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('', views.home, name='home'),  # Home page
-    path('register/', views.register, name='register'),  # Registration page
-    path('login/', views.login_view, name='login'),  # Login page
-    path('logout/', views.logout_view, name='logout'),  # Logout action
+    path('admin/', admin.site.urls),  # Admin panel URL
+    path('', include('myapp.urls')),  # Include the app's URLs
 ]
